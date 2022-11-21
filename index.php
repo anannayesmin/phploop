1ebf9c978e3e74cc5bfd0ffd084b78e57256c4dc
ananna <anannayesmin31@gmail.com>
2022-11-21 11:36:12 +0600

    project initalized

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+
+
+/*function even($num){
+    for($i = 2; $i <=$num; $i++ ){
+        if($i %2 == 0 ){
+            echo "even Number is" . $i . "<br>";
+        }
+    }
+}
+
+even(10);
+
+/*function odd($num){
+    for($i=1;$i <=$num;$i++){
+        if($i%2==1){ 
+            echo "odd number is " . $i . "<br>";
+        }
+    }
+}
+odd(10);*/
+
+//function even($num){
+   // $a = 1;
+    //while($a <=$num){
+       // if($a%2==0){
+           // echo "even number is". $a. "<br>";
+       // }
+   // $a++;
+   // }
+//}    
+//even(10);
+
+function odd($num){
+    $a=1;
+    while($a <= $num){
+        if($a%2==1){
+            echo "odd number is" . $a . "<br>";
+        }
+        $a++;
+    }
+}
+odd(10);
+
+//$a = 1;
+//do {
+    //echo $a . "<br>";
+     // $a++;
+    //} while ($a <= 10);
+
+/*function sum($a,$b,$c,$d){
+    echo $a+$b+$c+$d . "<br>";
+}
+sum(4,5,67,93);
+
+function sub($a,$b){
+    echo $a-$b . "<br>"; 
+}
+sub(20,15);
+
+function multi($a,$b,$c){
+    echo $a*$b*$c . "<br>";
+}
+multi(6,7,8);
+
+function mod($a,$b){
+    echo $a%$b;
+}
+mod(64,2);*/
+
+
+
+/*echo "Sum from variable". "<br>";
+$a = 4;
+$b = 5;
+
+echo $a + $b . "<br>";
+$a = 6;
+$b = 6;
+echo $a + $b . "<br>";
+
+$a = 76;
+$b = 6;
+echo $a + $b . "<br>";
+
+function sum($a, $b){
+    return ($a + $b). "<br>";
+}
+
+function subs($a, $b){
+ $c = $a - $b;
+ return $c;
+}
+
+
+echo "<br>"."Sum from function" . "<br>";
+echo sum(6,6);
+echo sum(4,5);
+echo sum(76,6);
+
+$val = subs(50,4);
+
+echo "Substraction: ". $val . "<br>";*/
+/*
+
+
+do {
+echo $a . "<br>";
+    $a++;
+} while ($a <= 10);
+
+//while loop
+//$a = 1;
+//while ($a <= 10) {
+
+    //if($a % 2 == 1){
+        //echo "This is even Number: $a"."<br>";
+    //}
+     
+
+   // $a++;
+//}
+
+//$a=1;
+//while ($a <=100){
+    //if($a%2==1){
+        //echo "$a" . "<br>";
+    //}
+     
+    // $a++;
+//}
+
+for($i=2;$i<=100;$i+=2){
+    echo $i . "<br>";
+}
+
+for($i=1;$i<=100;$i+=2){
+    echo $i . "<br>";
+}
+/*echo "Hello Word";
+echo "<br>";
+//vaiable
+
+$a=5;
+var_dump ($a);
+echo "<br>";
+$b=5.44;
+var_dump ($b);
+echo "<br>";
+$nam="Anu";
+var_dump ($nam);
+echo "<br>";
+$d=0;
+var_dump ($d);
+echo "<br>";
+
+
+$x=7;
+$y=9;
+$sum=$x=$y;
+echo "$sum";
+echo "<br>";
+$y=10;
+$z=5;
+$result=$y-$z;
+echo "$result";
+echo "<br>";
+$a=9;
+$b=9;
+$result=$a*$b;
+echo "$result";
+echo "<br>";
+$x=10;
+$y=11;
+$result=$x/$y;
+echo "$result";
+echo "<br>";*/
+//loop
+
+
+//for($i=2;$i<=10;$i+=2){
+  //  echo $i. "<br>";
+//}
+//index,,associated,,foreach
+//$namarray=['mim'=>22,'asfa'=>19,'fatima'=>21,'khushi'=>23];
+       //print_r($namarray);
+        //echo $namarray['mim'];
+    //foreach($namarray as $key=>$array){
+        //echo $key . "<br>";
+    //}
+ //$a=10;
+//echo($a>10)? "big 12":"something";  //ternary oporater
+
+
+//for
+
+//for($i=1;$i<=10;$i+=2){
+   // echo $i , "<br>";
+//}
+
+//$namarray=['anu'=>20,'chonda'=>23,'priya'=>22];
+         //print_r($namarray);
+         //echo $namarray[2];
+          //echo $namarray['anu']; //associated
+          //foreach($namarray as $nam=>$array){
+           // echo $nam . "<br>";
+         // }
+
+        // $result=75;
+         //echo($result>76)? "you have got 75": "something";
+
+         
+//$result=83;
+//echo($result>84)? "you have got 83": "something";
+
+
+/*
+
+$result=85;
+if($result>=80){
+    if($result>=85){
+        echo "your super result";
+    }
+}
+else{
+    echo "you have got A+";
+}
+
+*/
+
+
+
+//echo sum(90,90);
+?> 
+
+
+
+</body>
+</html> 
